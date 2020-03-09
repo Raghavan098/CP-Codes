a9fecfc842f974a1fcf44de3913c2b1935352bad
RaghavanGV <raghavan.gv@iiitb.org>
2020-03-09 11:38:58 +0530

    inf 4

---
+++ Infoarena/infoarena aiacubiti.cpp
@@ -0,0 +1,102 @@
+// g++ -std=c++14
+
+/*
+
+Today might be the chance to grasp the chance to let your talent bloom.
+May be tomorrow, the day after, or next year...
+May be even when you are 30. I'm not sure if physique has anything to do with it
+but if you think that it will never come, it probably never will.
+
+- Tooru Oikawa.
+
+*/
+
+
+#include<bits/stdc++.h>
+
+typedef long long ll;
+typedef long double lld;
+using namespace std;
+
+#define endl "\n"
+#define fi first
+#define se second
+#define pb(x) push_back(x)
+#define mp(x,y) make_pair(x,y)
+#define MEMS(a,b) memset(a,b,sizeof(a))
+#define _ ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define __ freopen("aiacubiti.in","r",stdin);freopen("aiacubiti.out","w",stdout);
+#define all(c) c.begin(),c.end()
+#define pii pair<int, int>
+#define tr(...) cout<<__FUNCTION__<<' '<<__LINE__<<" = ";trace(#__VA_ARGS__, __VA_ARGS__)
+template<typename S, typename T>
+ostream& operator<<(ostream& out,pair<S,T> const& p){out<<'('<<p.fi<<", "<<p.se<<')';return out;}
+
+template<typename T>
+ostream& operator<<(ostream& out,vector<T> const& v){
+ll l=v.size();for(ll i=0;i<l-1;i++)out<<v[i]<<' ';if(l>0)out<<v[l-1];return out;}
+
+template <typename T>
+ostream &operator<<(ostream &out, set<T> const &v) {
+    for (auto i = v.begin(); i != v.end(); i++)
+        out << (*i) << ' ';
+    return out;
+}
+template <typename T, typename V>
+ostream &operator<<(ostream &out, map<T, V> const &v) {
+    for (auto i = v.begin(); i != v.end(); i++)
+        out << "\n" << (i->first) <<  ":"  << (i->second);
+    return out;
+}
+
+template<typename T>
+void trace(const char* name, T&& arg1){cout<<name<<" : "<<arg1<<endl;}
+
+template<typename T, typename... Args>
+void trace(const char* names, T&& arg1, Args&&... args){
+const char* comma = strchr(names + 1, ',');cout.write(names, comma-names)<<" : "<<arg1<<" | ";trace(comma+1,args...);}
+
+
+const int N = (1 << 20) + 10;
+
+int n, arr[N];
+int dp[5][N];
+
+int popcount[N];
+
+int solve() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < 1024; i++) {
+        popcount[i] = __builtin_popcount(i);
+    }
+    ll ans = 0;
+    for (int i = 1; i <= n; i++) {
+        int up = ((arr[i] >> 10) << 10), low = (arr[i] & (1023));
+        for (int j = 0; j < 1024; j++) {
+            int d = popcount[low ^ j];
+            if (d <= 4) {
+                ans += dp[4 - d][up | j];
+            }
+        }
+        for (int j = 0; j < 1024; j++) {
+            int d = popcount[(up >> 10) ^ j];
+            if (d <= 4) {
+                dp[d][j << 10 | low]++;
+            }
+        }
+    }
+    cout << ans << endl;
+    return 0;
+
+}
+
+int32_t main(){ _
+__
+    int t;
+    // cin >> t;
+    t = 1;
+    while (t--) solve();
+}
